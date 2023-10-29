ae801fbfeabd361e3560cefb60ae83d53896e710
Alessandro Antonini <alessandro.antonini@sap.com>
2023-10-29 23:28:55 +0100

    wip

---
+++ configurable-cronjob/src/main/groovy/com/sap/cx/boosters/easy/configurablecronjob/job/ConfigurableJobPerformable.groovy
@@ -0,0 +1,21 @@
+package com.sap.cx.boosters.easy.configurablecronjob.job
+
+import com.sap.cx.boosters.easy.configurablecronjob.easytype.models.ConfigurableCronJobModel
+import de.hybris.platform.cronjob.enums.CronJobResult
+import de.hybris.platform.cronjob.enums.CronJobStatus
+import de.hybris.platform.servicelayer.cronjob.AbstractJobPerformable
+import de.hybris.platform.servicelayer.cronjob.PerformResult
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class ConfigurableJobPerformable extends AbstractJobPerformable<ConfigurableCronJobModel> {
+
+    static final Logger LOG = LoggerFactory.getLogger(ConfigurableJobPerformable.class)
+
+    @Override
+    PerformResult perform(ConfigurableCronJobModel cronJob) {
+        LOG.info('cronjob configuration : {}', cronJob.configuration)
+        return new PerformResult(CronJobResult.SUCCESS, CronJobStatus.FINISHED)
+    }
+
+}
---
+++ configurable-cronjob/src/models/groovy/com/sap/cx/boosters/easy/configurablecronjob/easytype/models/ConfigurableCronJobModel.groovy
@@ -0,0 +1,56 @@
+package com.sap.cx.boosters.easy.configurablecronjob.easytype.models
+
+import com.sap.cx.boosters.easy.configurablecronjob.easytype.enums.CronJobType
+import de.hybris.bootstrap.annotations.Accessor
+import de.hybris.platform.cronjob.model.CronJobModel
+import de.hybris.platform.servicelayer.model.ItemModelContext
+
+class ConfigurableCronJobModel extends CronJobModel {
+    
+    static final String _TYPECODE = ConfigurableCronJobModel.simpleName - 'Model'
+
+    static final String TYPE = 'type'
+
+    static final String PARAMETERS = 'parameters'
+
+    static final String MAX_DURATION = 'maxDuration'
+
+    ConfigurableCronJobModel() {
+        super()
+    }
+
+    ConfigurableCronJobModel(final ItemModelContext ctx) {
+        super(ctx)
+    }
+
+    @Accessor(qualifier = TYPE, type = Accessor.Type.GETTER)
+    CronJobType getType() {
+        return getPersistenceContext().getPropertyValue(TYPE)
+    }
+
+    @Accessor(qualifier = TYPE, type = Accessor.Type.SETTER)
+    void setType(final CronJobType value) {
+        getPersistenceContext().setPropertyValue(TYPE, value)
+    }
+
+    @Accessor(qualifier = PARAMETERS, type = Accessor.Type.GETTER)
+    Map<String, String> getParams() {
+        return getPersistenceContext().getPropertyValue(PARAMETERS)
+    }
+
+    @Accessor(qualifier = PARAMETERS, type = Accessor.Type.SETTER)
+    void setParams(final Map<String, String> value) {
+        getPersistenceContext().setPropertyValue(PARAMETERS, value)
+    }
+
+    @Accessor(qualifier = MAX_DURATION, type = Accessor.Type.GETTER)
+    Integer getMaxDuration() {
+        return getPersistenceContext().getPropertyValue(MAX_DURATION)
+    }
+
+    @Accessor(qualifier = MAX_DURATION, type = Accessor.Type.SETTER)
+    void setMaxDuration(final Integer value) {
+        getPersistenceContext().setPropertyValue(MAX_DURATION, value)
+    }
+
+}
