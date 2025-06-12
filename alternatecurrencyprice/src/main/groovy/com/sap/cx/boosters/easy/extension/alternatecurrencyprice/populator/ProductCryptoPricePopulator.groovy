e6219bd80b6cf234612911d72b41edaa9ee40904
Amit Panwar <amit.panwar@sap.com>
2025-06-12 19:37:21 +0100

    temporary attribute population

--- alternatecurrencyprice/src/main/groovy/com/sap/cx/boosters/easy/extension/alternatecurrencyprice/populator/ProductCryptoPricePopulator.groovy
+++ alternatecurrencyprice/src/main/groovy/com/sap/cx/boosters/easy/extension/alternatecurrencyprice/populator/ProductCryptoPricePopulator.groovy
@@ -1,7 +1,9 @@
 package com.sap.cx.boosters.easy.extension.alternatecurrencyprice.populator
 
 import com.sap.cx.boosters.easy.extension.alternatecurrencyprice.priceconversion.service.PriceConversionService
+import de.hybris.platform.acceleratorfacades.order.data.PriceRangeData
 import de.hybris.platform.commercefacades.product.converters.populator.AbstractProductPopulator
+import de.hybris.platform.commercefacades.product.data.PriceData
 import de.hybris.platform.commercefacades.product.data.ProductData
 import de.hybris.platform.core.model.c2l.CurrencyModel
 import de.hybris.platform.core.model.product.ProductModel
@@ -32,7 +34,11 @@ class ProductCryptoPricePopulator<SOURCE extends ProductModel, TARGET extends Pr
                 LOG.info("Base store has 'cryptoCurrency' property set. Populating crypto price for product: ${source.code}")
                 def cryptoCurrency = baseStore.cryptoCurrency as CurrencyModel
                 if(cryptoCurrency) {
-                    price.cryptoPrice = "${cryptoCurrency.symbol ?cryptoCurrency.symbol: cryptoCurrency.isocode} ${priceConversionService.getCryptoPrice(price.value, price.currencyIso, cryptoCurrency.isocode)}"
+                    def cryptoPriceValue = priceConversionService.getCryptoPrice(price.value, price.currencyIso, cryptoCurrency.isocode)
+                    price.cryptoPrice = "${cryptoCurrency.symbol ? cryptoCurrency.symbol : cryptoCurrency.isocode} ${cryptoPriceValue}"
+                    // Temporarily setting the name to include crypto price in formatted price for testing purposes
+                    LOG.info("Setting temprorary crypto price formatted value of price for product: ${source.code}")
+                    price.formattedValue = price.cryptoPrice
                     LOG.info("Crypto price populated for product: ${source.code} - Crypto Price: ${price.cryptoPrice}")
                 }else {
                     LOG.warn("Crypto currency is not set in base store for product: ${source.code}. Cannot populate crypto price.")
