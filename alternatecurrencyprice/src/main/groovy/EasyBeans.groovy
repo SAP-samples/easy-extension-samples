f06793e9767bae8ac33dbcf6ba457c6e05e81ed9
Amit Panwar <amit.panwar@sap.com>
2025-08-12 12:32:35 +0530

    optimized

--- alternatecurrencyprice/src/main/groovy/EasyBeans.groovy
+++ alternatecurrencyprice/src/main/groovy/EasyBeans.groovy
@@ -1,10 +1,7 @@
-import com.sap.cx.boosters.easy.extension.alternatecurrencyprice.mapper.impl.PriceMetaClassMapper
 import com.sap.cx.boosters.easy.extension.alternatecurrencyprice.populator.ProductCryptoPricePopulator
 import com.sap.cx.boosters.easy.extension.alternatecurrencyprice.priceconversion.service.impl.DefaultPriceConversionService
 import de.hybris.platform.commercefacades.product.data.PriceData
 import de.hybris.platform.commercewebservicescommons.dto.product.PriceWsDTO
-import de.hybris.platform.converters.Populator
-import de.hybris.platform.converters.impl.DefaultPopulatorList
 
 logger.info "[${extension.id}] registering Spring beans for ..."
 
@@ -13,31 +10,20 @@ println "Registering core beans for ${extension.id}"
 PriceData.metaClass.cryptoPrice = ''
 PriceWsDTO.metaClass.cryptoPrice = ''
 
-static def addPopulator(DefaultPopulatorList populatorList, Populator populator) {
-
-    def matches = populatorList.populators.findAll { it.class.name == populator.class.name }
-    populatorList.populators.removeAll(matches)
-    populatorList.populators.add(populator)
-}
-
 easyCoreBeans {
     priceConversionService(DefaultPriceConversionService)
 
-    productCryptoPricePopulator(ProductCryptoPricePopulator)
+    productCryptoPricePopulator(ProductCryptoPricePopulator) {
+        it.parent = ref('defaultProductPricePopulator')
+    }
+    registerAlias('productCryptoPricePopulator', 'productPricePopulator')
 
-    addPopulator(defaultProductPricePopulatorList as DefaultPopulatorList, productCryptoPricePopulator as Populator)
-
-    registerAlias('defaultProductPricePopulatorList', 'productPricePopulatorList')
 
 }
 
 easyWebBeans('/occ/springmvc-v2') {
     priceWsDTOFieldSetLevelMapping.levelMapping.DEFAULT = 'currencyIso,priceType,value,maxQuantity,minQuantity,formattedValue,cryptoPrice'
     priceWsDTOFieldSetLevelMapping.levelMapping.FULL = 'currencyIso,priceType,value,maxQuantity,minQuantity,formattedValue,cryptoPrice'
-
-    priceMetaClassMapper(PriceMetaClassMapper){
-        it.parent = ref('abstractCustomMapper')
-    }
 }
 
 logger.info "[${extension.id}] beans registered ..."
--- alternatecurrencyprice/src/main/groovy/com/sap/cx/boosters/easy/extension/alternatecurrencyprice/populator/ProductCryptoPricePopulator.groovy
+++ alternatecurrencyprice/src/main/groovy/com/sap/cx/boosters/easy/extension/alternatecurrencyprice/populator/ProductCryptoPricePopulator.groovy
@@ -3,6 +3,7 @@ package com.sap.cx.boosters.easy.extension.alternatecurrencyprice.populator
 import com.sap.cx.boosters.easy.extension.alternatecurrencyprice.priceconversion.service.PriceConversionService
 import de.hybris.platform.acceleratorfacades.order.data.PriceRangeData
 import de.hybris.platform.commercefacades.product.converters.populator.AbstractProductPopulator
+import de.hybris.platform.commercefacades.product.converters.populator.ProductPricePopulator
 import de.hybris.platform.commercefacades.product.data.PriceData
 import de.hybris.platform.commercefacades.product.data.ProductData
 import de.hybris.platform.core.model.c2l.CurrencyModel
@@ -14,7 +15,7 @@ import org.slf4j.LoggerFactory
 
 import javax.annotation.Resource
 
-class ProductCryptoPricePopulator<SOURCE extends ProductModel, TARGET extends ProductData> extends AbstractProductPopulator<SOURCE, TARGET> {
+class ProductCryptoPricePopulator<SOURCE extends ProductModel, TARGET extends ProductData> extends ProductPricePopulator<SOURCE, TARGET> {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProductCryptoPricePopulator)
 
@@ -26,20 +27,18 @@ class ProductCryptoPricePopulator<SOURCE extends ProductModel, TARGET extends Pr
 
     @Override
     void populate(SOURCE source, TARGET target) throws ConversionException {
-        LOG.info("Start :: Populating crypto price for product: ${source.code}")
+        super.populate(source, target)
+        LOG.debug("Start :: Populating crypto price for product: ${source.code}")
         def baseStore = baseStoreService.getCurrentBaseStore()
         if (baseStore.hasProperty('cryptoCurrency')) {
             def price = target.getPrice()
             if(null!=price){
-                LOG.info("Base store has 'cryptoCurrency' property set. Populating crypto price for product: ${source.code}")
+                LOG.debug("Base store has 'cryptoCurrency' property set. Populating crypto price for product: ${source.code}")
                 def cryptoCurrency = baseStore.cryptoCurrency as CurrencyModel
                 if(cryptoCurrency) {
                     def cryptoPriceValue = priceConversionService.getCryptoPrice(price.value, price.currencyIso, cryptoCurrency.isocode)
-                    price.cryptoPrice = "${cryptoCurrency.symbol ? cryptoCurrency.symbol : cryptoCurrency.isocode} ${cryptoPriceValue}"
-                    // Temporarily setting the name to include crypto price in formatted price for testing purposes
-                    LOG.info("Setting temprorary crypto price formatted value of price for product: ${source.code}")
-                    price.formattedValue = price.cryptoPrice
-                    LOG.info("Crypto price populated for product: ${source.code} - Crypto Price: ${price.cryptoPrice}")
+                    price['cryptoPrice'] = "${cryptoCurrency.symbol ? cryptoCurrency.symbol : cryptoCurrency.isocode} ${cryptoPriceValue}"
+                    LOG.debug("Crypto price populated for product: ${source.code} - Crypto Price: ${price['cryptoPrice']}")
                 }else {
                     LOG.warn("Crypto currency is not set in base store for product: ${source.code}. Cannot populate crypto price.")
                 }
@@ -47,8 +46,8 @@ class ProductCryptoPricePopulator<SOURCE extends ProductModel, TARGET extends Pr
                 LOG.warn("Price data is null for product: ${source.code}. Cannot populate crypto price.")
             }
         }else{
-            LOG.warn("Base store does not have 'cryptoCurrency' property set. Skipping crypto price population for product: ${source.code}")
+            LOG.debug("Base store does not have 'cryptoCurrency' property set. Skipping crypto price population for product: ${source.code}")
         }
-        LOG.info("End :: Populating crypto price for product: ${source.code}")
+        LOG.debug("End :: Populating crypto price for product: ${source.code}")
     }
 }
